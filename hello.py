8bf64c7bf0b0320f8324af0c162ca90dea9ec496
henrique Leao <henrique.saraiva@trt6.jus.br>
2022-08-23 14:11:11 -0300

    Adicionar requirements

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -8,7 +10,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html',name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)

8d8e91335ba65f60a5b214710c3afb0b53592e06
Henrique Leão <henrique.saraiva@gmail.com>
2022-08-25 16:53:32 -0300

    atualizacao do requirements

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+from flask_moment import Moment
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+moment = Moment(app)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
@@ -11,6 +13,14 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
 
 if __name__ == '__main__':
     app.run(debug=True)

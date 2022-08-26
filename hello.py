b6eea8a327fc23411cbe35513433da05463e6286
Henrique Leão <henrique.saraiva@gmail.com>
2022-08-26 18:12:55 -0300

    atualizacao do requirements

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_moment import Moment
 from flask_bootstrap import Bootstrap
 from datetime import datetime
@@ -17,12 +17,15 @@ class NameForm(FlaskForm):
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html',form=form, name=session['name'])
+    return render_template('index.html',form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):

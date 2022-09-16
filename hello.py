a9aa23f6b3deb9deaafb47fe947c45dbcc470f03
henrique Leao <henrique.saraiva@trt6.jus.br>
2022-09-16 10:52:16 -0300

    Adicionar requirements

--- hello.py
+++ hello.py
@@ -9,37 +9,37 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
+import sqlalchemy.dialects.sqlite
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='123456'
-# app.config['SQLALCHEMY_DATABASE_URI'] ='sqllite:///' + os.path.join(basedir, 'data.sqlite')
-# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
-# class Role(db.Model):
-#     __tablename__  = 'roles'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(64), unique=True)
+class Role(db.Model):
+    __tablename__  = 'roles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
 #
-#     users = db.relationship('User', backref='role')
-#
-#     def __repr__(self):
-#         return '<Role %r>' % self.name
-#
-# class User(db.Model):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(64), unique=True, index=True)
-#
-#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-#
-#     def __repr__(self):
-#         return '<User %r>' % self.username
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class NameForm(FlaskForm):
     name = StringField('Whats is your name?',validators=[InputRequired()])
